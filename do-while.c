806361c77a7cfbf897e50e44b18e0870de0ae401
Kaid02 <kabirS317@gmail.com>
2017-01-03 10:16:45 +0545

    Add files via upload

---
+++ do-while.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int a=1,b=1,c,n,i=1;
+	
+	printf("Enter the first n term to display : ");
+	scanf("%d",&n);
+	
+	printf("%d\t%d\t",a,b);
+	
+	while(i<=n)
+	{
+		
+		c=a+b;
+		printf("%d\t",c);
+		a=b;
+		b=c;
+		i++;
+		
+	}
+	
+	
+	return 0;
+}		
+		
